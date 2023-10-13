3d61ebf5b533aeb2a2613d1d537028ac8f2d0ab0
Andrew Kullman <agkullman@protonmail.com>
2023-10-12 23:38:42 -0800

    Console Log Added

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const slowBtn = document.querySelector('#slowButton');
 const goBtn = document.querySelector('#goButton');
 
 const stopLight = document.getElementById('stopLight');
+const slowLight = document.getElementById('slowLight')
+const goLight = document.getElementById('goLight')
 
 stopBtn.addEventListener('click', function(e) {
-  // console.log(e).target
-    stopLight.classList.add('stop')
+  stopLight.classList.toggle('stop');
+  if (stopLight.classList.contains('stop')) {
+    console.log(`${stopBtn.textContent} bulb on`);
+  } else{
+    console.log(`${stopBtn.textContent} bulb off`)
+  }
+});
+
+slowBtn.addEventListener('click', function(e){
+  slowLight.classList.toggle('slow')
+  if (slowLight.classList.contains('slow')){
+    console.log(`${slowBtn.textContent} bulb on`)
+  } else {
+    console.log(`${slowBtn.textContent} bulb off`)
+  }
+})
+
+goBtn.addEventListener('click', function(e)  {
+  goLight.classList.toggle('go')
+  if (goLight.classList.contains('go')){
+    console.log(`${goBtn.textContent} bulb on`)
+  } else {
+    console.log(`${goBtn.textContent} bulb off`)
+  }
 })
 
 
